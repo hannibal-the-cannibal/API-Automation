0db10828e8ccc606a8b33637818307544fb8ddaa
hannibal-the-cannibal <shreeshbhardwaj@gmail.com>
2025-10-18 11:09:50 +0530

    first Automation commit

---
+++ src/test/java/com/rest/Test.java
@@ -0,0 +1,15 @@
+package com.rest;
+
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.*;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+
+    public void test(){
+                given().
+                when().
+                then();
+    }
+}
